83a1704650b11dc235bf9f41e029eaf932e5f170
abuhassan <abu.a.hassan@gmail.com>
2024-06-18 22:21:04 +0800

    backend server and Mongodb complete

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config();
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+const port = process.env.PORT || 8000;
+import userRoutes from "./routes/userRoutes.js";
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => res.send("Server is Ready"));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`.cyan.underline);
+});
